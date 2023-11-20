ee61542673266f54d54aeb8e88776bc1c0ffee82
Lee Jae Yeon <wodus7792@naver.com>
2023-11-20 21:22:49 +0900

    메인 title.php 사용

--- php/main.php
+++ php/main.php
@@ -22,23 +22,8 @@ $loggedIn = isset($_SESSION['id']);
     <header>
         <h1>뭐해? 뮤해!</h1>
     </header>
-
-    <nav>
-        <a href="#">MUSICALS</a>
-        <a href="#">REVIEW</a>
-        <a href="#">LIKE</a>
-        <a href="myTicket.php">TICKETBOOK</a>
-        <a href="#">MYPAGE</a>
-        <?php
-        // 로그인되어 있는 경우에만 로그아웃 링크를 표시
-        if ($loggedIn) {
-            echo '<a href="logout.php">LOGOUT</a>';
-        }
-        else{
-            echo '<a href="login.php">LOGIN</a>';
-        }
-        ?>
-    </nav>
+    
+    <?php include 'title.php'; ?>
 
     <div class="content">
         <div class="left-side">
